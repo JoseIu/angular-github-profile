9c48da5e662d9c4449421790fb40db9878154bd9
Joselu <joseluissgz97@gmail.com>
2023-12-08 21:39:58 +0100

    appp makes the query for get userData for user-page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from './interfaces/user.interface';
+import { Repos } from './interfaces/userRepos.interface';
+import { UserProfileService } from './services/user-profile.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'github-profile';
+export class AppComponent implements OnInit {
+  public userData?: User;
+  public repos: Repos[] = [];
+
+  constructor(private userService: UserProfileService) {}
+  ngOnInit(): void {
+    this.userService.getDataUser().subscribe((user) => {
+      this.userData = user;
+      this.userService.getRepos(user.repos_url).subscribe((repos) => {
+        this.repos = repos;
+        console.log('REPOS', this.repos);
+      });
+
+      console.log('USERDATA', this.userData);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { UserProfileComponent } from './pages/user-profile/user-profile.component';
+import { daysPassedPipe } from './pipes/daysPassed.pipe';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
-  declarations: [AppComponent, UserProfileComponent, UserProfileComponent],
+  declarations: [
+    AppComponent,
+    UserProfileComponent,
+    UserProfileComponent,
+    daysPassedPipe,
+    SearchComponent,
+  ],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
