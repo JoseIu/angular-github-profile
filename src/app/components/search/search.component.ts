6e0d3b25428d2fddf6706c4c7ee14ee722d33704
Joselu <joseluissgz97@gmail.com>
2024-01-25 10:33:55 +0100

    debouncer

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { Subject, Subscription, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'search-input',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss'],
 })
-export class SearchComponent {}
+export class SearchComponent implements OnInit, OnDestroy {
+  private debouncer: Subject<string> = new Subject<string>();
+  private debouncerSubscription?: Subscription;
+  @Output() public onDebouncer = new EventEmitter<string>();
+  ngOnInit(): void {
+    this.debouncer
+      .pipe(debounceTime(300))
+      .subscribe((value) => this.onDebouncer.emit(value));
+  }
+  ngOnDestroy(): void {
+    this.debouncerSubscription?.unsubscribe;
+  }
+
+  public searchUser(event: any): void {
+    this.debouncer.next(event.target.value);
+  }
+}
