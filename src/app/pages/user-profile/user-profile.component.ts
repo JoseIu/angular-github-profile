70e9202618017c00bc956623228c7a5fa6beb206
Joselu <joseluissgz97@gmail.com>
2024-01-25 17:25:08 +0100

    subscribing to userData for each serach

--- src/app/pages/user-profile/user-profile.component.ts
+++ src/app/pages/user-profile/user-profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/interfaces/user.interface';
 import { Repos } from 'src/app/interfaces/userRepos.interface';
 import { UserProfileService } from 'src/app/services/user-profile.service';
@@ -8,39 +8,39 @@ import { UserProfileService } from 'src/app/services/user-profile.service';
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.scss'],
 })
-export class UserProfileComponent {
-  @Input() public userData?: User;
-  @Input() public repos: Repos[] = [];
+export class UserProfileComponent implements OnInit {
+  public userData?: User;
+  public repos: Repos[] = [];
 
   public isLoading: boolean = true;
 
   constructor(private userService: UserProfileService) {}
+
   ngOnInit(): void {
     this.userService.getDataUser().subscribe((user) => {
       this.userData = user;
 
-      this.getRepos(user.name);
-      this.isLoading = false;
-
-      console.log('USERDATA', this.userData);
+      if (user) {
+        this.getRepos(user.login);
+        this.isLoading = false;
+      }
     });
-  }
 
-  public searchUser(term: string): void {
-    console.log(term);
-    this.userService.getDataUser(term).subscribe((user) => {
-      this.userData = user;
-      this.getRepos(user.repos_url);
-      this.isLoading = false;
-      console.log(user);
+    this.userService.userData.subscribe({
+      next: (user) => {
+        if (user) {
+          this.userData = user;
+          this.getRepos(user.login);
+          this.isLoading = false;
+        }
+      },
+      error: console.error,
     });
   }
 
   public getRepos(name: string): void {
     this.userService.getRepos(name).subscribe((repos) => {
       this.repos = repos;
-
-      console.log('REPOS', this.repos);
     });
   }
 }
