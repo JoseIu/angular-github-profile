167940e145a6cc5b6bcce8b3ccee6f86affaa079
Joselu <joseluissgz97@gmail.com>
2024-01-25 17:24:21 +0100

    removing Output

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,11 +1,7 @@
-import {
-  Component,
-  EventEmitter,
-  OnDestroy,
-  OnInit,
-  Output,
-} from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { Subject, Subscription, debounceTime } from 'rxjs';
+import { User } from 'src/app/interfaces/user.interface';
+import { UserProfileService } from 'src/app/services/user-profile.service';
 
 @Component({
   selector: 'search-input',
@@ -13,13 +9,27 @@ import { Subject, Subscription, debounceTime } from 'rxjs';
   styleUrls: ['./search.component.scss'],
 })
 export class SearchComponent implements OnInit, OnDestroy {
+  public userService = inject(UserProfileService);
+
   private debouncer: Subject<string> = new Subject<string>();
   private debouncerSubscription?: Subscription;
-  @Output() public onDebouncer = new EventEmitter<string>();
+
+  public userData?: User;
+  public usertFound: boolean = true;
+
   ngOnInit(): void {
-    this.debouncer
-      .pipe(debounceTime(300))
-      .subscribe((value) => this.onDebouncer.emit(value));
+    this.debouncer.pipe(debounceTime(300)).subscribe((value) =>
+      this.userService.getDataUser(value).subscribe((user) => {
+        //Cheack if user is not undefined
+        if (user !== undefined) {
+          this.usertFound = true;
+          this.userData = user;
+          return;
+        }
+        this.usertFound = false;
+        console.log('SEARCH', user);
+      })
+    );
   }
   ngOnDestroy(): void {
     this.debouncerSubscription?.unsubscribe;
@@ -27,5 +37,10 @@ export class SearchComponent implements OnInit, OnDestroy {
 
   public searchUser(event: any): void {
     this.debouncer.next(event.target.value);
+  }
+  public setUser(): void {
+    if (this.userData) {
+      this.userService.upDateUserData(this.userData);
+    }
   }
 }
