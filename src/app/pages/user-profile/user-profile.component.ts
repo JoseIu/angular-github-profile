139184bd8db4d44353a67fdb687a3255486c0f34
Joselu <joseluissgz97@gmail.com>
2024-01-25 12:02:52 +0100

    moving the logic to the user-component

--- src/app/pages/user-profile/user-profile.component.ts
+++ src/app/pages/user-profile/user-profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { User } from 'src/app/interfaces/user.interface';
 import { Repos } from 'src/app/interfaces/userRepos.interface';
+import { UserProfileService } from 'src/app/services/user-profile.service';
 
 @Component({
   selector: 'user-profile',
@@ -10,4 +11,36 @@ import { Repos } from 'src/app/interfaces/userRepos.interface';
 export class UserProfileComponent {
   @Input() public userData?: User;
   @Input() public repos: Repos[] = [];
+
+  public isLoading: boolean = true;
+
+  constructor(private userService: UserProfileService) {}
+  ngOnInit(): void {
+    this.userService.getDataUser().subscribe((user) => {
+      this.userData = user;
+
+      this.getRepos(user.name);
+      this.isLoading = false;
+
+      console.log('USERDATA', this.userData);
+    });
+  }
+
+  public searchUser(term: string): void {
+    console.log(term);
+    this.userService.getDataUser(term).subscribe((user) => {
+      this.userData = user;
+      this.getRepos(user.repos_url);
+      this.isLoading = false;
+      console.log(user);
+    });
+  }
+
+  public getRepos(name: string): void {
+    this.userService.getRepos(name).subscribe((repos) => {
+      this.repos = repos;
+
+      console.log('REPOS', this.repos);
+    });
+  }
 }
