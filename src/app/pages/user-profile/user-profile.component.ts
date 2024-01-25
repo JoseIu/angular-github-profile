8d7d2a4232081002d5af43bc0253719fa5f59b4c
Joselu <joseluissgz97@gmail.com>
2024-01-25 17:39:04 +0100

    destroy$ for finish all subscritions

--- src/app/pages/user-profile/user-profile.component.ts
+++ src/app/pages/user-profile/user-profile.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
 import { User } from 'src/app/interfaces/user.interface';
 import { Repos } from 'src/app/interfaces/userRepos.interface';
 import { UserProfileService } from 'src/app/services/user-profile.service';
@@ -8,25 +9,29 @@ import { UserProfileService } from 'src/app/services/user-profile.service';
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.scss'],
 })
-export class UserProfileComponent implements OnInit {
+export class UserProfileComponent implements OnInit, OnDestroy {
   public userData?: User;
   public repos: Repos[] = [];
 
   public isLoading: boolean = true;
+  private destroy$ = new Subject<void>();
 
   constructor(private userService: UserProfileService) {}
 
   ngOnInit(): void {
-    this.userService.getDataUser().subscribe((user) => {
-      this.userData = user;
+    this.userService
+      .getDataUser()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((user) => {
+        this.userData = user;
 
-      if (user) {
-        this.getRepos(user.login);
-        this.isLoading = false;
-      }
-    });
+        if (user) {
+          this.getRepos(user.login);
+          this.isLoading = false;
+        }
+      });
 
-    this.userService.userData.subscribe({
+    this.userService.userData.pipe(takeUntil(this.destroy$)).subscribe({
       next: (user) => {
         if (user) {
           this.userData = user;
@@ -37,7 +42,10 @@ export class UserProfileComponent implements OnInit {
       error: console.error,
     });
   }
-
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
   public getRepos(name: string): void {
     this.userService.getRepos(name).subscribe((repos) => {
       this.repos = repos;
