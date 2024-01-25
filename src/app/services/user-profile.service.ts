4d0b0cbe470882a0d6a703ebb0daa13843c95003
Joselu <joseluissgz97@gmail.com>
2024-01-25 17:22:38 +0100

    retur undefined if we have no user

--- src/app/services/user-profile.service.ts
+++ src/app/services/user-profile.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, catchError, of } from 'rxjs';
 import { User } from '../interfaces/user.interface';
 import { Repos } from '../interfaces/userRepos.interface';
 
@@ -10,13 +10,25 @@ import { Repos } from '../interfaces/userRepos.interface';
 export class UserProfileService {
   private baseUrl = `https://api.github.com/users`;
 
+  public userData = new BehaviorSubject<User | undefined>(undefined);
+
   constructor(private http: HttpClient) {}
 
-  public getDataUser(username: string = 'github'): Observable<User> {
-    return this.http.get<User>(`${this.baseUrl}/${username}`);
+  public getDataUser(
+    username: string = 'github'
+  ): Observable<User | undefined> {
+    return this.http.get<User>(`${this.baseUrl}/${username}`).pipe(
+      catchError((error) => {
+        console.error(error);
+        return of(undefined);
+      })
+    );
   }
 
   public getRepos(name: string): Observable<Repos[]> {
     return this.http.get<Repos[]>(`${this.baseUrl}/${name}/repos`);
+  }
+  public upDateUserData(user: User): void {
+    this.userData.next(user);
   }
 }
