e89c6f27db7ec314341cedf80891947a625e58f5
Joselu <joseluissgz97@gmail.com>
2024-01-25 12:00:51 +0100

    moving the entire logic to the user-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { User } from './interfaces/user.interface';
-import { Repos } from './interfaces/userRepos.interface';
-import { UserProfileService } from './services/user-profile.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  public userData?: User;
-  public repos: Repos[] = [];
-  public isLoading: boolean = true;
-
-  constructor(private userService: UserProfileService) {}
-  ngOnInit(): void {
-    this.userService.getDataUser().subscribe((user) => {
-      this.userData = user;
-
-      this.getRepos(user.repos_url);
-      this.isLoading = false;
-
-      console.log('USERDATA', this.userData);
-    });
-  }
-
-  public searchUser(term: string): void {
-    console.log(term);
-    this.userService.getDataUser(term).subscribe((user) => {
-      this.userData = user;
-      this.getRepos(user.repos_url);
-      this.isLoading = false;
-      console.log(user);
-    });
-  }
-
-  public getRepos(url: string): void {
-    this.userService.getRepos(url).subscribe((repos) => {
-      this.repos = repos;
-
-      console.log('REPOS', this.repos);
-    });
-  }
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SearchComponent } from './components/search/search.component';
-import { UserProfileComponent } from './pages/user-profile/user-profile.component';
-import { daysPassedPipe } from './pipes/daysPassed.pipe';
-import { RepoCardComponent } from './components/repo-card/repo-card.component';
-import { LoaderComponent } from './components/loader/loader.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    UserProfileComponent,
-    UserProfileComponent,
-    daysPassedPipe,
-    SearchComponent,
-    RepoCardComponent,
-    LoaderComponent,
-  ],
-  imports: [BrowserModule, HttpClientModule, ReactiveFormsModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
